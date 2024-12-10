397b8c212ec5d5dceda16971b26fd406f8d1971f
Avinda Shamal <shamalgba.21@uom.lk>
2024-12-10 16:20:11 +0530

    Addressed review comments

---
+++ avinda_shamal/test/unit/test_cifar_len.py
@@ -0,0 +1,19 @@
+import pytest
+from unittest.mock import patch
+from src.cifar_class import CustomImageDataset
+ 
+@pytest.mark.parametrize("datalist, expected", [
+    ([("a.jpg", "horse"), ("b.jpg", "frog"), ("c.jpg", "dog"), ("d.jpg", "cat")], 4),
+    ([("a.jpg", "horse"), ("b.jpg", "frog"), ("c.jpg", "dog")], 3),
+    ([("a.jpg", "horse"), ("b.jpg", "frog")], 2),
+    ([("a.jpg", "horse")], 1)
+])
+
+def test_len_custom_dataset(datalist, expected):
+        
+    with patch("src.cifar_class.CustomImageDataset.__init__", return_value=None):
+        dataset = CustomImageDataset(root_dir="mock_path", transform=None)
+        dataset.data = datalist
+
+        # test the len method
+        assert len(dataset) == expected
