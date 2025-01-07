7de24711fa3d8cabcbea722d227f612ea55fd052
AvindaShamal <avindashamal@gmail.com>
2025-01-07 20:33:52 +0530

    updated unit test

--- avinda_shamal/test/unit/test_eval_model.py
+++ avinda_shamal/test/unit/test_eval_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 from model import ModelEvaluator
+from unittest.mock import patch
 import numpy as np
 
 
@@ -13,23 +14,21 @@ def test_test_model():
             [0.65, 0.20, 0.09, 0.06],
             [0.27, 0.15, 0.03, 0.55],
             [0.1, 0.71, 0.18, 0.01],
-            [0.65, 0.20, 0.09, 0.06],
-            [0.27, 0.15, 0.03, 0.55],
-            [0.1, 0.71, 0.18, 0.01],
-            [0.65, 0.20, 0.09, 0.06],
-            [0.27, 0.15, 0.03, 0.55],
-            [0.1, 0.71, 0.18, 0.01],
         ]
     )
-    test_labels = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    test_labels = torch.tensor([0, 1, 2, 3])
     dummy_dataset = TensorDataset(test_data, test_labels)
     test_loader = DataLoader(dummy_dataset, batch_size=1, shuffle=False)
-    evaluator = ModelEvaluator(nn.Linear(4, 10), test_loader)
-    # Test the method
-    metrics = evaluator.test_model()
-    # Validate metrics
-    assert metrics["Accuracy"] >= 0
-    assert isinstance(metrics["confusion_matrix"], np.ndarray)
-    assert "precision" in metrics and metrics["precision"] > 0
-    assert metrics["recall"] > 0
-    assert metrics["f1_score"] > 0
+    with patch("model.ModelEvaluator.roc_auc") as mock_roc_auc:
+        mock_roc_auc.return_value = {"micro": 0.5}
+        evaluator = ModelEvaluator(nn.Linear(4, 4), test_loader)
+        # Test the method
+        metrics = evaluator.test_model()
+        # Validate metrics
+        assert metrics["Accuracy"] >= 0
+        assert isinstance(metrics["confusion_matrix"], np.ndarray)
+        assert "precision" in metrics and metrics["precision"] > 0
+        assert metrics["recall"] > 0
+        assert metrics["f1_score"] > 0
+        assert "roc_auc" in metrics
+        print("All tests passed!")
